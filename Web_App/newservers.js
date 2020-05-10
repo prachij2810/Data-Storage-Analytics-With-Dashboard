6ffb4360f80867101b7c2ecbf4192410d6ae38be
tommotaku <pepito.tip@gmail.com>
2020-05-10 23:08:47 +0530

    stylised print format

--- Web_App/newservers.js
+++ Web_App/newservers.js
@@ -1,3 +1,24 @@
+var modal = document.getElementById('id01'); 
+      window.onclick = function(event) { 
+          if (event.target == modal) { 
+              modal.style.display = "none"; 
+          }
+          
+      }
+      var btnclick = document.getElementById('smt');
+            if(btnclick)
+              btnclick.addEventListener('click', fnc);
+          function fnc(e){
+             e.preventDefault();
+            var uname = document.getElementById("uname");
+            var pw = document.getElementById("pw");
+            //alert(pw.value);
+            if(uname.value == "admin" && pw.value == "admin"){
+              //alert(pw.value);
+              modal.style.display = "none";
+              var para = document.createElement("P");                 // Create a <p> element
+              para.innerHTML = "Log in successful!";                // Insert text
+              document.getElementById("logg").appendChild(para);
 btn = document.getElementById("btn");
 var dist1, dist;
 btn.addEventListener('click', (e)=>{
@@ -15,11 +36,41 @@ function setCharAt(str,index,chr) {
     return str.substr(0,index) + chr + str.substr(index+1);
 }
 var fragment = document.createDocumentFragment();
-var table = document.createElement("table");
+var table = document.getElementById("tbl");
 var i = dist1.length-1;
 dist1 = setCharAt(dist1, i, c);
     var ref = firebase.database().ref("district/user");
     var body = document.getElementsByTagName("body")[0];
+    var rowth = document.createElement("tr");
+    var th = document.createElement("th");
+    var thText = document.createTextNode('Name');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('Fathers Name');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('DOB');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('ID');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('Roll No');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('School');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    var th = document.createElement("th");
+    var thText = document.createTextNode('District');
+    th.appendChild(thText);        
+    rowth.appendChild(th);
+    table.appendChild(rowth);
     ref.orderByKey().startAt(dist).endAt(dist1).on("child_added", function(data, prevChildKey) {
         var newPlayer = data.val();
         var row = document.createElement("tr");
@@ -63,3 +114,11 @@ dist1 = setCharAt(dist1, i, c);
  fragment.appendChild(table);
  body.appendChild(fragment);
 });
+            }
+            else{
+                modal.style.display = "none";
+                var para = document.createElement("P");                 // Create a <p> element
+                para.innerHTML = "Username or Password incorrect";                // Insert text
+                document.getElementById("logg").appendChild(para);
+              }
+        }
